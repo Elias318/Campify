74bf21ba375d15730db8e8d1d0bb55aaf8d9658e
Elias Olivencia <elias.olivencia@gmail.com>
2025-01-21 00:22:25 -0300

    Cambios

--- resources/views/meGustas.blade.php
+++ resources/views/meGustas.blade.php
@@ -8,25 +8,21 @@
 <div class="carrito-container">
     <!-- Vista de la tabla -->
     <div id="listaMeGustas" class="container-fluid w-correccionMegustas my-5">
-        <div class="row border bg-light">
-            {{-- <div class="col-2 px-2 text-center">Imagen</div> --}}
-            <div class="col-3 px-2 text-center">Nombre</div>
-            <div class="col-3 px-2 text-center">Categoría</div>
-            <div class="col-3 px-2 text-center">Acciones</div>
+        <div class="row border bg-light text-center">
+            <div class="col-4 px-2">Nombre</div>
+            <div class="col-4 px-2">Categoría</div>
+            <div class="col-4 px-2"></div>
         </div>
         @foreach ($productos as $meGusta)
-            <div class="row border align-items-center productoMegustas-{{$meGusta->producto_id}}">
-                {{-- <div class="col-2 text-center">
-                    <img src="{{ secure_asset('storage/' . $meGusta->producto->imagen_producto) }}" alt="Imagen del producto" style="width: 80px; height: 80px;">
-                </div> --}}
-                <div class="col-3 text-center">{{ $meGusta->producto->nombre_producto }}</div>
-                <div class="col-3 text-center">{{ $meGusta->producto->categoria->nombre_categoria }}</div>
-                <div class="botonMeGusta col-3 text-center justify-content-center d-flex">
+            <div class="row border align-items-center productoMegustas-{{$meGusta->producto_id}} text-center">
+                <div class="col-4">{{ $meGusta->producto->nombre_producto }}</div>
+                <div class="col-4">{{ $meGusta->producto->categoria->nombre_categoria }}</div>
+                <div class="col-4">
                     <form method="post" action="{{ route('me-gusta.destroy') }}">
                         @csrf
                         @method("DELETE")
                         <input type="hidden" name="producto_id" value="{{$meGusta->producto_id}}">
-                        <button type="submit" class="d-flex justify-content-center align-items-center btn_corazonRoto">
+                        <button type="submit" class="btn_corazonRoto">
                             <i class="fa-solid fa-heart-crack"></i>
                         </button>
                     </form>
@@ -36,9 +32,9 @@
     </div>
 
     <!-- Vista de las tarjetas -->
-    <div id="tarjetasMeGustas" class="row d-flex justify-content-center align-items-center mx-2">
+    <div id="tarjetasMeGustas" class="row d-flex justify-content-center mx-2">
         @foreach ($productos as $meGusta)
-            <div class="col-12 col-sm-6 col-lg-4 mb-4 d-flex justify-content-center align-items-center mt-5 productoMegustas-{{$meGusta->producto_id}}">
+            <div class="col-12 col-sm-6 col-lg-4 mb-4 d-flex justify-content-center productoMegustas-{{$meGusta->producto_id}}">
                 <div class="tarjeta-producto">
                     <img src="{{ asset('storage/' . $meGusta->producto->imagen_producto) }}" alt="Imagen del producto">
                     <div class="container_datos_tarjeta">
@@ -50,7 +46,7 @@
                                     @csrf
                                     @method("DELETE")
                                     <input type="hidden" name="producto_id" value="{{$meGusta->producto_id}}">
-                                    <button type="submit" class="d-flex justify-content-center align-items-center btn_corazonRoto">
+                                    <button type="submit" class="btn_corazonRoto">
                                         <i class="fa-solid fa-heart-crack"></i>
                                     </button>
                                 </form>
@@ -62,5 +58,6 @@
         @endforeach
     </div>
 </div>
+
 
 @include('footer')
